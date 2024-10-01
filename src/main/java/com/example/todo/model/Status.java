fb4b240ad100f505565074030bcf0f38c610aa72
fred.nobre <fred.nobre@amarn25y6f6vh.lan>
2024-10-01 10:14:40 +0200

    add class todo and enum Status

---
+++ src/main/java/com/example/todo/model/Status.java
@@ -0,0 +1,10 @@
+package com.example.todo.model;
+
+public enum Status {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+
+
+}
---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,19 @@
+package com.example.todo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Todo {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private Date createdAT;
+}
