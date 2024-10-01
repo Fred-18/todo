ec36f67c3cc7cfa18cf86f690a08566514737470
fred.nobre <fred.nobre@amarn25y6f6vh.lan>
2024-10-01 17:38:08 +0200

    add constructor in TodoEntity add todoService

--- src/main/java/com/example/todo/model/TodoEntity.java
+++ src/main/java/com/example/todo/model/TodoEntity.java
@@ -5,7 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
 import java.util.Date;
+
 
 @Entity
 public class TodoEntity {
@@ -16,4 +18,50 @@ public class TodoEntity {
     private String description;
     private Status status;
     private Date createdAT;
+
+    public TodoEntity(String title,String description,Status status,Date createAT){
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.createdAT = createAT;
+
+    }
+
+    public TodoEntity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public Date getCreatedAT() {
+        return createdAT;
+    }
+    public void setCreatedAT(Date createdAT) {
+        this.createdAT = createdAT;
+    }
+
 }
